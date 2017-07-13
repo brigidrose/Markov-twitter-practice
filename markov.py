e00cfb2d2f290de7d97c2304b84fb4ec272b662d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-12 17:21:16 -0700

    Working Markov chain twitter bot.

--- markov.py
+++ markov.py
@@ -9,7 +9,6 @@ def open_and_read_file(filenames):
         string."""
 
     body = ""
-
     for filename in filenames:
         text_file = open(filename)
         body = body + text_file.read()
@@ -45,7 +44,10 @@ def make_text(chains):
 
     key = choice(chains.keys())
     words = [key[0], key[1]]
+    char_count = len(key[0]) + len(key[1]) + 1
+    
     while key in chains:
+
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text)
         #
@@ -53,17 +55,30 @@ def make_text(chains):
         # it would run for a very long time.
 
         word = choice(chains[key])
+        char_count += len(word) + 1
+        if char_count > 140:
+            break
         words.append(word)
         key = (key[1], word)
-
+        
+    
     return " ".join(words)
+    
 
-
-def tweet(chains):
+def tweet(text):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    pass
+    api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])   
+    #print api.VerifyCredentials()
+    status = api.PostUpdate(text)
+    print "twitter is happy"
+
+   
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
@@ -74,6 +89,9 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
-
+random_text = make_text(chains)
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+print len(random_text)
+print random_text
+tweet(random_text)
+
